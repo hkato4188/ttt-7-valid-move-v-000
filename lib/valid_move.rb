710cd86b233cafadb92e58fc8bec0aae201eecd3
Hiroki Kato <hkato@sas.upenn.edu>
2018-01-23 22:21:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,6 @@ def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     true
   elsif board[index] != " " || board[index] != "   " || board[index] == nil
-    true
   else
     false
   end
